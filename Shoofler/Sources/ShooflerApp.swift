3ae540d302f0364fdbdb0314c32880abe8c05704
Xavier Michelon <xavier@michelon.ch>
2025-04-25 19:45:55 +0200

    WIP: keyboard monitoring.

--- Shoofler/Sources/ShooflerApp.swift
+++ Shoofler/Sources/ShooflerApp.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @main
 struct ShooflerApp: App {
+    private var monitor: Any?
     static let mainWIndowID = "shoofler-main-window"
     @AppStorage("theme") private var theme: Theme = .system
     
@@ -9,8 +10,14 @@ struct ShooflerApp: App {
         Window("Shoofler", id: ShooflerApp.mainWIndowID) {
             ContentView()
                 .preferredColorScheme(theme.colorScheme)
+                .onAppear {
+                    NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { event in
+                        let path = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first!.appendingPathComponent("keyboard.log")
+                        do { try "A key was pressed".write(to: path, atomically: true, encoding: String.Encoding.utf8) } catch {}
+                        print("A key was pressed")
+                    }
+                }
         }
-        
         MenuBarExtra("Shoofler", image: "MenuBarIcon") {
             MenuBarContentView()
         }
